b4181c213c0dfc201bd839de887f7ddccd28d34e
Bratanov <111681891+ybratanov@users.noreply.github.com>
2022-10-14 20:26:27 +0200

    Update 07. Spiral Matrix.js

--- FUNDAMENTALS WITH JAVASCRIPT - Септември 2022/07.1 Arrays More Exercise/07. Spiral Matrix.js
+++ FUNDAMENTALS WITH JAVASCRIPT - Септември 2022/07.1 Arrays More Exercise/07. Spiral Matrix.js
@@ -1,20 +1,56 @@
-function solve(num1, num2) {
-  let max = num1 * num2;
+function spiralMatrix(row, col) {
+ 
   let result = [];
-  let top = 1;
-  let right = 1;
-  let botton = 1;
-  let left = 1;
-
-  for (let i = 1; i <= max; i++) {
-    if(top<=5){
-        result.push[i]
-        top++
-    }else if
-    (result.length >= 5) {
-      console.log(result.join(" "));
-      result=[]
-    }
+  let counter = 1;
+  let startCol = 0;
+  let endCol = col - 1;
+  let startRow = 0;
+  let endRow = row - 1;
+ 
+  for (let i = 0; i < row; i++) {
+ 
+    result.push([]);
+ 
   }
+  while (startCol <= endCol && startRow <= endRow) {
+    for (let i = startCol; i <= endCol; i++) {
+ 
+      result[startRow][i] = counter;
+ 
+      counter++;
+ 
+    }
+ 
+    startRow++;
+    for (let i = startRow; i <= endRow; i++) {
+ 
+      result[i][endCol] = counter;
+      counter++;
+ 
+    }
+    endCol--;
+ 
+    for (let i = endCol; i >= startCol; i--) {
+ 
+      result[endRow][i] = counter;
+ 
+      counter++;
+ 
+    }
+ 
+    endRow--;
+ 
+    for (let i = endRow; i >= startRow; i--) {
+ 
+      result[i][startCol] = counter;
+ 
+      counter++;
+ 
+    }
+ 
+    startCol++;
+  }
+  result = result.forEach(row => console.log(row.join(' ')));
+ 
 }
-solve(5, 5);
+spiralMatrix(5 ,5)
